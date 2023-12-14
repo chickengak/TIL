01e5f08de21687936bbbeed4a9a563b537bc6d25
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2023-12-14 23:30:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5191.83 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,5 +1,3 @@
-#from itertools import *
-
 def solution(elements):
     comb_sum = set(elements)
     elements = elements*2
@@ -8,12 +6,3 @@ def solution(elements):
             comb_sum.add(sum(elements[i:i+n]))
     
     return len(comb_sum)+1
-    
-    # comb_sum = set(elements)
-    # comb_sum.add(sum(elements))
-    # combs = []
-    # for n in range(2, len(elements), 1):
-    #     combs += set(combinations(elements, n))
-    # for comb in combs:
-    #     comb_sum.add(sum(comb))
-    # return sorted(comb_sum)
