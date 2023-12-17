e0e00760df0dceb9fb2177d04b59578514149399
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2023-12-17 19:39:47 +0900

    [level 2] Title: [1차] 캐시, Time: 1.11 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,19 @@
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    if cacheSize == 0:
+        return len(cities)*5
+    
+    for c in cities:
+        city = c.lower()
+        try:
+            idx = cache.index(city)
+            answer += 1
+            cache = cache[:idx]+cache[idx+1:] +[city]
+        except:
+            answer += 5
+            if len(cache) < cacheSize:
+                cache.append(city)
+            else:
+                cache = cache[1:] + [city]
+    return answer
