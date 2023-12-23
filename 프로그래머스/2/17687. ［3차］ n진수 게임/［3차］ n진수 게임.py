8520f8d08359429b11ee2048298fcdcaf52f5834
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2023-12-23 20:12:06 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.38 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,15 @@
+def solution(n, t, m, p):
+    answer = '0'
+    dic = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+    start = 1
+    while len(answer) < t*m:
+        temp = ''
+        share = start
+        while True:
+            share, remainder = divmod(share, n)
+            temp += dic[remainder]
+            if share == 0:
+                break
+        answer += temp[::-1]
+        start +=1
+    return answer[p-1::m][:t]
