38aeebf467a7ef19a92445caf6eb6cc3ab1e6506
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2023-12-30 15:39:09 +0900

    [level 2] Title: 더 맵게, Time: 1627.70 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -6,11 +6,9 @@ def solution(scoville, K):
     
     while scoville[0] < K:
         if len(scoville) < 2:
-            break
+            return -1
         a = heapq.heappop(scoville)
         b = heapq.heappop(scoville)
         heapq.heappush(scoville, b*2+a)
         answer += 1
-    else:
-        return answer
-    return -1
+    return answer
