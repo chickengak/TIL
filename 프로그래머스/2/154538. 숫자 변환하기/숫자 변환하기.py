cdeffbaf1ea431cfa6158cf3aae7e3069ecf3dd9
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2024-01-03 19:07:46 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,14 +1,12 @@
 def solution(x, y, n):
     answer = 0
     cur_set = {x}
-    stop = max(20, (y-x)//n)
     while y not in cur_set:
-
         next_set = set()
         while cur_set:
             num = cur_set.pop()
-            temp = [num+n, num*2, num*3]
-            next_set.update([t for t in temp if t <= y])
+            calc_list = [num+n, num*2, num*3]
+            next_set.update([calc for calc in calc_list if calc <= y])
         cur_set = next_set
         if not cur_set or min(cur_set) > y:
             return -1
